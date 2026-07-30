b76e377390e6f799b5f9b4d3400679d4b7c175ed
Clément Pit-Claudel <clement.pit-claudel@epfl.ch>
2026-07-31 01:31:43 +0200

    build: Fix `make sandbox` by generating boogie-friends-pkg.el

--- emacs/scripts/package-name.el
+++ emacs/scripts/package-name.el
@@ -1,8 +1,16 @@
 (load (expand-file-name "common.el" (file-name-directory load-file-name)))
 
-(defconst my-package-version (with-temp-buffer
-                               (insert-file-contents (expand-file-name "../boogie-friends-pkg.el" base-directory))
-                               (concat "boogie-friends-" (nth 2 (read (current-buffer))))))
+(require 'package)
+
+(defconst my-package-desc (with-temp-buffer
+                            (insert-file-contents (expand-file-name "../boogie-friends.el" base-directory))
+                            (package-buffer-info)))
+
+(defconst my-package-version (package-desc-full-name my-package-desc))
 
 (defun my-package-version ()
   (princ my-package-version))
+
+(defun my-write-pkg-file ()
+  (package-generate-description-file
+   my-package-desc (expand-file-name "boogie-friends-pkg.el" (pop command-line-args-left))))
